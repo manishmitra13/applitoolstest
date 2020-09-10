e3753d06b76403af48da9690ba61caa31281b7d6
User <user@192-168-1-119.tpgi.com.au>
2020-09-11 00:24:16 +1000

    Added Changes for Docker

---
+++ cypress/applitools.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apiKey: '5CrrnlgsziPZqV8PRuaNEZTCyIGd2MIFVv24Thgreec110',
+    browser: [
+        {width: 2000, height: 800, name: 'chrome'},
+        {width: 2000, height: 800, name: 'firefox'},
+        {width: 2000, height: 800, name: 'ie11'},
+        {width: 2000, height: 800, name: 'edgechromium'},
+        {width: 2000, height: 800, name: 'safari'},
+    ],
+    batchName: 'TestCheck'
+  }
--- cypress/integration/examples/AddingItems.spec.js
+++ cypress/integration/examples/AddingItems.spec.js
@@ -10,18 +10,35 @@ describe('Navigation to the product to product and trying to checkout a product'
   })
 
   it('Perform a shop online', () => {
+    cy.eyesOpen({
+      appName: 'AppOne',
+      testName: 'Check for Fruit and Veg',
+    })
+
     cy.contains('Fruit & Veg').click();
     cy.get(selectors.Fruit).click();
     cy.contains('Grapes').click();
 
+    cy.eyesCheckWindow({
+      tag: "GrapesSection",
+      target: 'window',
+      fully: true
+  });
     //Validation only Grapes to be shown
     cy.get(selectors.allGrapesItems).each(($el, index, $list) => {
       cy.get($el).then(() => {
-          expect($el.text()).to.contain("Grapes");
+        expect($el.text()).to.contain("Grapes");
       })
     })
     cy.get(selectors.addtoCartGrapes).click();
+
+    cy.eyesCheckWindow({
+      tag: "CheckoutSection",
+      target: 'window',
+      fully: true
+  });
     cy.contains('Checkout').click();
+    cy.eyesClose();
   })
 
 });
