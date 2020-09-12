43cbc7f6b14ebfc29946b057e21ebec4745a1821
User <user@192-168-1-119.tpgi.com.au>
2020-09-12 21:29:23 +1000

    More changes

--- cypress/integration/fillingupthesignOut.spec..js
+++ cypress/integration/fillingupthesignOut.spec..js
@@ -20,21 +20,21 @@ describe('Navigation to the product to product and trying to checkout a product'
       fully: true
     });
 
-    cy.contains('Log in / Signup').click();
-    cy.contains('New to Woolworths online').click();
+    cy.contains('Log in / Signup').click({force: true});
+    cy.contains('New to Woolworths online').click({force: true});
     cy.get(selectors.firstName).type("ManishAppliTest");
     cy.get(selectors.lastName).type("ManishAppliTest");
     cy.get(selectors.emailName).type("sample@sample.com");
     cy.get(selectors.password).type("Manish01");
     cy.get(selectors.dateofBirth).type("11/11/1990");
     cy.get(selectors.prefferedContactNumber).type("0451878192");
-    cy.get(selectors.clickNo).click();
-    cy.get(selectors.YesConditions).click();
+    cy.get(selectors.clickNo).click({force: true});
+    cy.get(selectors.YesConditions).click({force: true});
     cy.eyesCheckWindow({
       tag: "Form Before Signup_New",
       fully: true
     });
-    cy.get(selectors.SignUp).click();
+    cy.get(selectors.SignUp).click({force: true});
     cy.get('.formValidationSummary-text').contains("The sign up was not successful.");
 
     cy.eyesClose()
